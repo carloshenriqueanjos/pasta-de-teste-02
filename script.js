4790aa34b0fc1c8ec0b192ebd5e812198f7cde08
Carlos Henrique dos Anjos Santos <104277476+carloshenriqueanjos@users.noreply.github.com>
2022-09-03 15:29:57 -0300

    Update script.js
    update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const tbody = document.createElement('tbody');
 
-const x = {"fisrtName":["carlos","maria", "joao"]};
+const nomes = {"fisrtName":["carlos","maria", "joao"]};
 
 function table() {
     let tr = document.createElement('tr');
 
     tr.innerHTML = `
-    <td>${x.fisrtName[0]}</td>
-    <td>${x.fisrtName[1]}</td>
-    <td>${x.fisrtName[2]}</td>
+    <td>${nomes.fisrtName[0]}</td>
+    <td>${nomes.fisrtName[1]}</td>
+    <td>${nomes.fisrtName[2]}</td>
     `
     tbody.appendChild(tr);
 }
