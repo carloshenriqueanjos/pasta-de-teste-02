67956b40fb6c3302047bfba1e288420dcb2d485a
Carlos Henrique dos Anjos Santos <104277476+carloshenriqueanjos@users.noreply.github.com>
2022-09-05 08:49:21 -0300

    Update script.js
    update

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const nomes = {"fisrtName":["carlos","maria", "joao"]};
 
 function table() {
-    let dados;
+    let table = document.createElement("table");
 
-    dados.innerHTML = `
-    <table>
+    table.innerHTML = `
     <tr>
     <th>${nomes.fisrtName[0]}</th>
     <th>${nomes.fisrtName[1]}</th>
     <th>${nomes.fisrtName[2]}</th>
     <tr>
-    <table>
     `
 }
 
